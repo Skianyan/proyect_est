0d8ad6289422ddbebcf26b2e9a36d8c9ecde0f77
Skianyan <99053383+Skianyan@users.noreply.github.com>
2023-05-24 12:00:47 -0700

    change page add tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@
 import { useState } from "react"
 
 const Page = () => {
-  const [nombre,setNombre] = useState('Haro Calvo Ricardo');
-  const [contador, setContador] = useState(0)
+  const [edad,setEdad] = useState(0);
+  const [nombre,setNombre] = useState('');
+  const [mensaje,setMensaje] = useState('');
+  const [contador, setContador] = useState(0);
 
   function contar(){
     setContador(contador + 1);
@@ -13,9 +15,20 @@ const Page = () => {
     setContador(0)
   }
 
+  function validar(){
+    if(edad>=18)
+      setMensaje('Es mayor de edad!')
+    else
+      setMensaje('No pasa :(')
+  }
+  const formHandler = (e:any) => {
+    e.preventDefault();
+    console.log('boton presionado')
+  }
+
   return (
     <div>
-      <h1>Proyecto Final</h1>
+      <h1 className="text-3xl text-center">Proyecto Final</h1>
       <h2>Estructuras de Datos</h2>
       <h3>{nombre}</h3>
       <button onClick={contar}>
@@ -25,6 +38,29 @@ const Page = () => {
       <button onClick={limpiar}>
         Borrar
       </button>
+
+      <form
+        onSubmit={formHandler}
+      >
+        <input 
+        type="text" 
+        placeholder="Nombre " 
+        value={nombre} 
+        onChange={(event)=> setNombre(event.target.value)}
+        />
+        <input type="submit" />
+        
+        <input 
+        type="text" 
+        placeholder="Edad " 
+        value={edad} 
+        onChange={(event)=> setEdad(parseInt(event.target.value))}
+        />
+      <button onClick={validar}>
+        Validar
+      </button>
+        <p>{mensaje}</p>
+      </form>
     </div>
   )
 }
